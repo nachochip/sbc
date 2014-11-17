5dbddb22205be3e989d2f1ef5eec8e924774d8c4
nachochip <blockchaincolony@gmail.com>
2014-11-17 13:57:36 -0600

    adjusting files

--- localrun.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#run a Docker image with local volume
-
-
-sudo docker run -v /home/ripena/projects/sbc/20141101-CFC-ec2-files/www:/www -it --entrypoint="bash" rplocalbuild/test:0.1
-
-#nachochip/ffmpeg:2.4.2
