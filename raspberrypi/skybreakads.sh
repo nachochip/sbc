4753684acc59daa5075559e3e4139ef41075cfa5
nachochip <blockchaincolony@gmail.com>
2017-01-15 11:49:12 -0600

    add new script, rpi ads display

---
+++ raspberrypi/skybreakads.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+#pkill skybreakads.sh
+#killall eog
+#sleep 30
+#sudo -u pi eog --slide-show /home/pi/skybreak-ads/ --display=':0' --fullscreen
+eog --slide-show /home/pi/Pictures/skybreak-ads/ --display=':0' --fullscreen
+#eog --slide-show /home/pi/mounted-drives/covenantshare/skybreak-ads-only --display=':0' --fullscreen
