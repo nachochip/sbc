a6bac438ca1c84523f8e669d00196212494c4f53
nachochip <blockchaincolony@gmail.com>
2017-04-02 11:58:11 -0500

    adjustments

--- raspberrypi/skybreak-wifi-persistence.sh
+++ raspberrypi/skybreak-wifi-persistence.sh
@@ -2,12 +2,25 @@
 #  This script checks the wifi and reconnects to appropriate one if not.
 #while true; do ./your-script & sleep 5; done
 
-TARGETSSID="skybreak"
+UNDESIREDSSID="Public"
+TARGETSSID="Skybreak"
+TARGETROUTE="10.0.0.1"
 
-route | grep $TARGETSSID
+iwgetid -r | grep $UNDESIREDSSID
 if [ $? -eq 0 ] ; then
-	echo "connected"
+	sudo ifdown wlan0 & sleep 3 & sudo ifup --force wlan0
 else
-	sudo ifdown wlan0
-	sudo ifup wlan0
+	iwgetid -r | grep $TARGETSSID
+	if [ $? -eq 0 ] ; then
+		route | grep $TARGETROUTE
+			if [ $? -eq 0 ] ; then
+				echo "should be ok"
+			else
+				dhclient wlan0
+			fi
+	else
+		sudo ifdown wlan0 & sleep 3 & sudo ifup --force wlan0
+	fi
 fi
+
+
