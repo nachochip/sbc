15b1df70381de0cec0a0f2098dc37644bb0f87dc
nachochip <blockchaincolony@gmail.com>
2017-04-09 11:46:31 -0500

    change name of items

--- raspberrypi/skybreakcamera-display-autostart.sh
+++ raspberrypi/skybreakcamera-display-autostart.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 # This script will start up the cameras with the proper settings.
 
+#clear all previous
+killall raspivid
+
 # this command changes the framrate from 60 fps to 59.94 fps
 tvservice -t -e "CEA 5"
 
