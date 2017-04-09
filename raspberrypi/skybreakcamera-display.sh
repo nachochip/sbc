508bc1e88d6591f2b25f6cb3ad5667c5aae068b5
nachochip <blockchaincolony@gmail.com>
2017-04-09 10:37:13 -0500

    adding camera command

---
+++ raspberrypi/skybreakcamera-display.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# This script will start up the cameras with the proper settings.
+
+# this command changes the framrate from 60 fps to 59.94 fps
+tvservice -t -e "CEA 5"
+
+# this command starts the video
+raspivid -f -t 0 -awb fluorescent -ISO 100
