7e288cf9771cdcc4138b064c8cb8dd1089cdb6a2
nachochip <blockchaincolony@gmail.com>
2019-07-16 17:03:23 -0500

    adjust

--- broadcasting/archivesettings-temp.sh
+++ broadcasting/archivesettings-temp.sh
@@ -8,7 +8,8 @@ read NAMEOFFILETOUSE
 # values that should not be changing much
 BASICVIDEOSETTINGS="-vcodec libx264 -pix_fmt yuv420p -aspect 16:9"
 BASICAUDIOSETTINGS="-acodec libfdk_aac"
-PATHSETTINGS="/home/ripena/Videos"
+PATHSETTINGSIN="/home/broadcast/mntforencoding/Input"
+PATHSETTINGSOUT="/home/broadcast/mntforencoding/Output"
 MESSAGEAUDIOSETPODCAST="-acodec libmp3lame -ar 48k -b:a 160k"
 WORSHIPAUDIOSETFORSHARE="-acodec libmp3lame -ar 48k -b:a 320k"
 
@@ -22,9 +23,9 @@ WORSHIPVIDEOSET="-preset superfast -s 1920x1080 -profile:v high -level:v 4.2 -b:
 WORSHIPAUDIOSET="-ar 48k -b:a 320k"
 
 
-ffmpeg -i $PATHSETTINGS/$NAMEOFFILETOUSE.mkv \
+ffmpeg -i $PATHSETTINGSIN/$NAMEOFFILETOUSE.mkv \
 		$BASICVIDEOSETTINGS $SERVICEARCHIVEVIDEOSET -pass 1 -passlogfile 1stlog -an -f mp4 -y /dev/null && \
-	ffmpeg -i $PATHSETTINGS/$NAMEOFFILETOUSE.mkv \
+	ffmpeg -i $PATHSETTINGSIN/$NAMEOFFILETOUSE.mkv \
 		$BASICVIDEOSETTINGS $SERVICEARCHIVEVIDEOSET -pass 2 -passlogfile 1stlog \
-		$BASICAUDIOSETTINGS $SERVICEARCHIVEAUDIOSET $PATHSETTINGS/${NAMEOFFILETOUSE}-archive-service.mp4 && \
+		$BASICAUDIOSETTINGS $SERVICEARCHIVEAUDIOSET $PATHSETTINGSOUT/${NAMEOFFILETOUSE}-archive-service.mp4 && \
         rm 1stlog*
